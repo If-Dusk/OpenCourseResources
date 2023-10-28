be9a4784113fe50ae6f6c364d2fdc9a85abdf717
If-Dusk <95753681+If-Dusk@users.noreply.github.com>
2023-10-28 16:20:56 +0800

    Delete 专业必修课/汇编语言 directory

--- 专业必修课/汇编语言/src/p186.asm
+++
@@ -1,7 +0,0 @@
-assume cs:code
-code segment
-start:	jmp short s
-	db 128 dup(0)
-    s:	mov ax,0ffffh
-code ends
-end start
--- 专业必修课/汇编语言/src/p82_1.asm
+++
@@ -1,9 +0,0 @@
-assume cs:code
-code segment
-	mov ax,2
-	add ax,ax
-
-	mov ax,4c00h
-	int 21h
-code ends
-end
